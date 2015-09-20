97aee7cb2ac6ef4adc36d8e2ebee117138a2168a
wasd35 <efekan.tuzcuoglu@ogr.sakarya.edu.tr>
2015-09-21 01:27:49 +0300

    Birkaç ekleme...

--- app/src/main/java/com/geoquiz/QuizActivity.java
+++ app/src/main/java/com/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,11 +12,13 @@ import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
+    private static final String KEY_INDEX = "index";
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
     private Button mPreviousButton;
     private TextView mQuestionTextView;
+    private static final String TAG = "QuizActivity";
 
     private Question[] mQuestionBank = new Question[] {
             new Question(R.string.question_oceans, true),
@@ -46,6 +49,7 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
@@ -96,14 +100,63 @@ public class QuizActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+                        if(mCurrentIndex == 0)
+                            mCurrentIndex = mQuestionBank.length;
                         mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
                         updateQuestion();
                     }
                 }
         );
 
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
         updateQuestion();
     }
+
+    /**************************************************************************************/
+    /**********Ekran orientation'u değişmesi gibi durumlarda activity sonlanıyor***********/
+    /***böyle bir durumda yeni yaratılan activitye değerler bu fonksiyon ile aktarılıyor***/
+    /**************************************************************************************/
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+    /**************************************************************************************/
+
+    /***************************************************************/
+    /***Bu kısım logcat üzerinden activity lifecycle izlemek için***/
+    /***************************************************************/
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+    /******************************---******************************/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
